acf39d485865490204a27f20ca19ec466afe6f0a
Kait <kweishaar@hawk.iit.edu>
2021-08-02 12:44:06 -0500

    first couple tests

--- _tests_/Emplyee.test.js
+++
@@ -1,7 +0,0 @@
-const constructEmployee = require('../index.js')
-test('checks if employee is created correctly', () => {
-    const sarah = new constructEmployee('Sarah');
-    expect(sarah.name).toBe('Sarah');
-    expect(sarah.id).toBe(1);
-    expect(sarah.email).toBe('sarah@gmail.com')
-})
